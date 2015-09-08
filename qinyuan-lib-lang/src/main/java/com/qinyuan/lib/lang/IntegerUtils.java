cbef977ff0c3cd96263fbf520b7d2c9a0c33d337
qinyuan <qinyuan15@sina.com>
2015-09-08 16:54:00 +0800

    modify IntegerUtils

--- qinyuan-lib-lang/src/main/java/com/qinyuan/lib/lang/IntegerUtils.java
+++ qinyuan-lib-lang/src/main/java/com/qinyuan/lib/lang/IntegerUtils.java
@@ -1,6 +1,10 @@
 package com.qinyuan.lib.lang;
 
+import com.google.common.collect.Lists;
 import org.apache.commons.lang3.math.NumberUtils;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Utility class for integer
@@ -21,5 +25,32 @@ public class IntegerUtils {
 
     public static boolean isNotNegative(String string) {
         return NumberUtils.isNumber(string) && isNotNegative(NumberUtils.toInt(string));
+    }
+
+    public static int getMaxContinuationSize(Iterable<Integer> integers) {
+        List<Integer> list = Lists.newArrayList(integers);
+        Collections.sort(list);
+
+        Integer lastValue = null;
+        int maxContinuationSize = 1, currentContinuationSize = 1;
+        for (Integer value : list) {
+            if (value == null) {
+                continue;
+            }
+
+            if (lastValue != null) {
+                if (value - lastValue == 1) {
+                    currentContinuationSize++;
+                    if (currentContinuationSize > maxContinuationSize) {
+                        maxContinuationSize = currentContinuationSize;
+                    }
+                } else if (value - lastValue > 1) {
+                    currentContinuationSize = 1;
+                }
+            }
+            lastValue = value;
+        }
+
+        return maxContinuationSize;
     }
 }
--- qinyuan-lib-lang/src/test/java/com/qinyuan/lib/lang/IntegerUtilsTest.java
+++ qinyuan-lib-lang/src/test/java/com/qinyuan/lib/lang/IntegerUtilsTest.java
@@ -1,6 +1,9 @@
 package com.qinyuan.lib.lang;
 
+import com.google.common.collect.Lists;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -42,5 +45,20 @@ public class IntegerUtilsTest {
         assertThat(IntegerUtils.isNotNegative("0")).isTrue();
         assertThat(IntegerUtils.isNotNegative("-1")).isFalse();
         assertThat(IntegerUtils.isNotNegative((String) null)).isFalse();
+    }
+
+    @Test
+    public void testGetMaxContinuationSize() throws Exception {
+        List<Integer> integers = Lists.newArrayList(1, 2, 3, 5, 6, 7, 8, 10);
+        assertThat(IntegerUtils.getMaxContinuationSize(integers)).isEqualTo(4);
+
+        integers = Lists.newArrayList(1, 2, 3, 5, 6, 7, 8, 10, 3, 6);
+        assertThat(IntegerUtils.getMaxContinuationSize(integers)).isEqualTo(4);
+
+        integers = Lists.newArrayList(1, 5, 6, 7, 8, 10, 3, 2);
+        assertThat(IntegerUtils.getMaxContinuationSize(integers)).isEqualTo(4);
+
+        integers = Lists.newArrayList(-1, -2, -3, -4, 1, 2, 4, 3, 4, 7, 8);
+        assertThat(IntegerUtils.getMaxContinuationSize(integers)).isEqualTo(4);
     }
 }
