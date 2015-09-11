4f6d880cfd705b311055dd4ce9d83c3218950e72
qinyuan <qinyuan15@sina.com>
2015-09-11 21:43:39 +0800

    add class FrequencyCounter

---
+++ qinyuan-lib-lang/src/main/java/com/qinyuan/lib/lang/FrequencyCounter.java
@@ -0,0 +1,47 @@
+package com.qinyuan.lib.lang;
+
+import java.util.*;
+
+/**
+ * count the frequency of certain objects appear
+ * Created by qinyuan on 15-9-11.
+ */
+public class FrequencyCounter {
+    private final Map<Object, Integer> counter = new HashMap<>();
+
+    public synchronized void add(Object value) {
+        if (counter.containsKey(value)) {
+            counter.put(value, counter.get(value) + 1);
+        } else {
+            counter.put(value, 1);
+        }
+    }
+
+    public List<Object> top(int n) {
+        Map<Integer, List<Object>> map = new TreeMap<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1; // order by key descend
+            }
+        });
+        for (Map.Entry<Object, Integer> entry : counter.entrySet()) {
+            List<Object> list = map.get(entry.getValue());
+            if (list == null) {
+                list = new ArrayList<>();
+                map.put(entry.getValue(), list);
+            }
+
+            list.add(entry.getKey());
+        }
+
+        List<Object> result = new ArrayList<>();
+        for (Map.Entry<Integer, List<Object>> entry : map.entrySet()) {
+            if (result.size() + entry.getValue().size() > n) {
+                return result;
+            } else {
+                result.addAll(entry.getValue());
+            }
+        }
+        return result;
+    }
+}
---
+++ qinyuan-lib-lang/src/test/java/com/qinyuan/lib/lang/FrequencyCounterTest.java
@@ -0,0 +1,28 @@
+package com.qinyuan.lib.lang;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FrequencyCounterTest {
+    @Test
+    public void testTop() throws Exception {
+        FrequencyCounter counter = new FrequencyCounter();
+
+        counter.add("1");
+        counter.add("1");
+        counter.add("2");
+        counter.add("2");
+        counter.add("2");
+        counter.add("3");
+        counter.add("3");
+        counter.add("4");
+
+        assertThat(counter.top(0)).isEmpty();
+        assertThat(counter.top(1)).containsExactly("2");
+        assertThat(counter.top(2)).containsExactly("2");
+        assertThat(counter.top(3)).containsExactly("2", "3", "1");
+        assertThat(counter.top(4)).containsExactly("2", "3", "1", "4");
+        assertThat(counter.top(5)).containsExactly("2", "3", "1", "4");
+    }
+}
