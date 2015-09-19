208ac70fea97babab93e68e471ca120b8758b435
qinyuan <qinyuan15@sina.com>
2015-09-19 13:56:08 +0800

    add class UserRole

---
+++ qinyuan-lib-mvc/src/main/java/com/qinyuan/lib/mvc/security/UserRole.java
@@ -0,0 +1,8 @@
+package com.qinyuan.lib.mvc.security;
+
+public class UserRole {
+    public final static String ADMIN = "ROLE_ADMIN";
+    public final static String NORMAL = "ROLE_NORMAL";
+    private UserRole() {
+    }
+}
