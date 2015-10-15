8e5d7f83164ee573c45ab913f6abd645e354dbc4
qinyuan <qinyuan15@sina.com>
2015-10-15 12:36:28 +0800

    add class CDNSource

---
+++ qinyuan-lib-mvc/src/main/java/com/qinyuan/lib/mvc/controller/CDNSource.java
@@ -0,0 +1,6 @@
+package com.qinyuan.lib.mvc.controller;
+
+public class CDNSource {
+    public final static String BOOTSTRAP_JS = "http://libs.baidu.com/bootstrap/3.3.4/js/bootstrap.min.js";
+    public final static String HANDLEBARS = "http://libs.baidu.com/handlebars.js/1.3.0/handlebars.min.js";
+}
