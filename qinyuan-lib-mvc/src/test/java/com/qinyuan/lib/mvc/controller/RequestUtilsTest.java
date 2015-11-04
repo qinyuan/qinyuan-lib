f268c6163e7b48c8981687f2304ab7c05598837b
qinyuan <qinyuan15@sina.com>
2015-11-04 21:49:24 +0800

    modify RequestUtilsTest

--- qinyuan-lib-mvc/src/test/java/com/qinyuan/lib/mvc/controller/RequestUtilsTest.java
+++ qinyuan-lib-mvc/src/test/java/com/qinyuan/lib/mvc/controller/RequestUtilsTest.java
@@ -12,11 +12,15 @@ public class RequestUtilsTest {
     @Test
     public void testGetRealRemoteAddress() throws Exception {
         HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getRemoteAddr()).thenReturn("127.0.0.1");
 
         when(request.getHeader("x-forwarded-for")).thenReturn("111.206.241.75,111.206.241.75, 183.61.236.115");
         assertThat(RequestUtils.getRealRemoteAddress(request)).isEqualTo("111.206.241.75");
 
         when(request.getHeader("x-forwarded-for")).thenReturn("113.92.82.187");
         assertThat(RequestUtils.getRealRemoteAddress(request)).isEqualTo("113.92.82.187");
+
+        when(request.getHeader("x-forwarded-for")).thenReturn(null);
+        assertThat(RequestUtils.getRealRemoteAddress(request)).isEqualTo("127.0.0.1");
     }
 }
