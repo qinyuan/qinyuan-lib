5f1de879885f2104d0014b0ae7691bb4220534b5
qinyuan <qinyuan15@sina.com>
2015-11-05 10:33:58 +0800

    add ResponseUtils

---
+++ qinyuan-lib-mvc/src/main/java/com/qinyuan/lib/mvc/controller/ResponseUtils.java
@@ -0,0 +1,45 @@
+package com.qinyuan.lib.mvc.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+public class ResponseUtils {
+    private final static Logger LOGGER = LoggerFactory.getLogger(ResponseUtils.class);
+
+    private ResponseUtils() {
+    }
+
+    public static void outputImage(HttpServletResponse response, String imagePath) {
+        try {
+            File file = new File(imagePath);
+            if (!file.isFile()) {
+                return;
+            }
+
+            response.setContentType("image/jpeg;charset=UTF8");
+
+            InputStream is = new FileInputStream(file);
+            BufferedInputStream bis = new BufferedInputStream(is);
+            OutputStream output = response.getOutputStream();
+            BufferedOutputStream bos = new BufferedOutputStream(output);
+
+            byte data[] = new byte[4096];
+            int size;
+            while ((size = bis.read(data)) != -1) {
+                bos.write(data, 0, size);
+            }
+
+            bis.close();
+            is.close();
+            bos.flush();
+            bos.close();
+            output.close();
+        } catch (Exception e) {
+            LOGGER.error("fail to print image,image path: {}, info: {}", imagePath, e);
+        }
+    }
+}
