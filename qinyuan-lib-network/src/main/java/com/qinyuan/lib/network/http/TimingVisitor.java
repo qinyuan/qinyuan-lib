71e8de5e99c2fc180be287d255c92bca933c99c8
qinyuan <qinyuan15@sina.com>
2016-01-11 18:02:43 +0800

    add class TimingVisitor

---
+++ qinyuan-lib-network/src/main/java/com/qinyuan/lib/network/http/TimingVisitor.java
@@ -0,0 +1,52 @@
+package com.qinyuan.lib.network.http;
+
+import com.qinyuan.lib.lang.concurrent.ThreadUtils;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Class to visit certain url by given interval regularly
+ * Created by qinyuan on 16-1-11.
+ */
+public class TimingVisitor {
+    public final static int DEFAULT_INTERVAL = 60;  // default interval is 1 minute
+    private int interval = DEFAULT_INTERVAL;
+    private String pages;
+
+    public void setInterval(int interval) {
+        this.interval = interval;
+    }
+
+    public void setPages(String pages) {
+        this.pages = pages;
+    }
+
+    public void run() {
+        (new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    ThreadUtils.sleep(interval);
+                    try {
+                        if (StringUtils.isBlank(pages)) {
+                            continue;
+                        }
+
+                        String[] urls = pages.split(",");
+                        for (String url : urls) {
+                            if (isLocalUrl(url)) {
+                                url = " http://localhost/" + url;
+                            }
+                            new HttpClient().get(url);
+                        }
+                    } catch (Throwable e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private boolean isLocalUrl(String url) {
+        return !url.startsWith("http://") && !url.startsWith("wwww.");
+    }
+}
---
+++ qinyuan-lib-network/src/test/java/com/qinyuan/lib/network/http/TimingVisitorTest.java
@@ -0,0 +1,15 @@
+package com.qinyuan.lib.network.http;
+
+import com.qinyuan.lib.lang.concurrent.ThreadUtils;
+import org.junit.Test;
+
+public class TimingVisitorTest {
+    @Test
+    public void testRun() throws Exception {
+        TimingVisitor visitor = new TimingVisitor();
+        visitor.setPages("http://www.bud-vip.com/commodity.html,http://www.bud-vip.com/index.html");
+        visitor.setInterval(1);
+        visitor.run();
+        ThreadUtils.sleep(10);
+    }
+}
